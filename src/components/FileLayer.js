4101a7a0e3b46a2a1f78cb5767ced8f1b5cf785f
jtmccormick18 <josh.mccormick81@gmail.com>
2022-11-23 10:48:08 -0500

    initial

---
+++ src/components/FileLayer.js
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import L from "leaflet";
+import { useMap } from "react-leaflet";
+import togeojson from "togeojson";
+import fileLayer from "leaflet-filelayer";
+
+fileLayer(null, L, togeojson);
+
+const style = {
+  color: "red",
+  opacity: 1.0,
+  fillOpacity: 1.0,
+  weight: 2,
+  clickable: true
+};
+
+export default function FileLayer() {
+  const map = useMap();
+
+  useEffect(() => {
+    const control = L.Control.fileLayerLoad({
+      fitBounds: true,
+      layerOptions: {
+        style: style,
+        pointToLayer: function (data, latlng) {
+          return L.circleMarker(latlng, { style: style });
+        }
+      }
+    });
+    control.addTo(map);
+    control.loader.on("data:loaded", function (e) {
+      var layer = e.layer;
+      console.log(layer);
+    });
+  }, [map]);
+
+  return null;
+}
